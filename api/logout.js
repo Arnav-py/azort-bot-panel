5ab790d3d360d0f7904924cd14f190e7a239a01b
Arnav-py <arnavsbv2@gmail.com>
2026-07-25 18:33:10 +0530

    Delete api directory

--- api/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearSessionCookie } = require('../lib/store');
-
-module.exports = async (req, res) => {
-  res.setHeader('Set-Cookie', clearSessionCookie('azort_session'));
-  return res.status(200).json({ ok: true });
-};
